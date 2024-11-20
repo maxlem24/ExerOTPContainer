e5c0f7612184af0e793da19e8dea4c53abc70d55
maxlem24 <lemaitre.maxime2004@gmail.com>
2024-11-20 15:51:20 +0100

    config radius

--- init/init_script.sh
+++ init/init_script.sh
@@ -30,6 +30,23 @@ echo "search exer.fr" >> /etc/resolv.conf
 
 bash /usr/local/bin/wizard.sh
 
+# Freeradius
+
+# Linking available <=> enabled
+ln -s /etc/freeradius/mods-available/* /etc/freeradius/mods-enabled
+ln -s /etc/freeradius/sites-available/* /etc/freeradius/sites-enabled
+
+# Linking with 3.0
+rm -r /etc/freeradius/3.0/*
+
+ln -s /etc/freeradius/radiusd.conf /etc/freeradius/3.0/radiusd.conf
+ln -s /etc/freeradius/clients.conf /etc/freeradius/3.0/clients.conf
+
+ln -s /etc/freeradius/mods-enabled /etc/freeradius/3.0/mods-enabled
+ln -s /etc/freeradius/mods-config /etc/freeradius/3.0/mods-config
+ln -s /etc/freeradius/sites-enabled /etc/freeradius/3.0/sites-enabled
+ln -s /etc/freeradius/policy.d /etc/freeradius/3.0/policy.d
+
 /etc/init.d/apache2 restart
 /etc/init.d/freeradius restart
 
